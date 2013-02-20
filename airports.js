31475763afe64538b05f4b17747c8b6b75b1692c
Finlay McAfee <finsqm@gmail.com>
2013-02-20 10:26:07 +0000

    includes d3, jquery and style files along with the globe and the countries

---
+++ airports.js
@@ -0,0 +1,3 @@
+window.airportData = {"type":"FeatureCollection","features":[
+{"type":"Feature","properties":{"name":"circle1"},"geometry":{"type":"Polygon","coordinates":[[-3.3725,55.9500],[-45.0000,55.9500],[-3.3725,54.0000]]},"id":"1"}
+]}
